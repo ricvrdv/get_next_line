91626dacf3d02d8aacc19f5c77a2d99a3ddf87da
Ricardo Jorge Jesus Da Costa Garcia <rjesus-d@c2r1s7.42porto.com>
2024-11-20 18:00:03 +0000

    Created source file and header

--- get_next_line.c
+++ get_next_line.c
@@ -1,7 +1,23 @@
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    //OPEN FILE
-    buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-    if
+	char	*buffer;
 
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		free(left_c);
+		free(buffer);
+		left_c = NULL;
+		buffer = NULL;
+		return (NULL);
+	}
+	if (!buffer)
+		return (NULL);
+	line = //FUNCTION TO FILL LINE
+	free(buffer);
+	buffer = NULL;
+	if (!line)
+		return (NULL);
+	left_c = //FUNCTION TO HANDLE LEFTOVERS
+	return (line);
 }
