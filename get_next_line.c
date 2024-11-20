d33cb732df9692f7d02a1fb606d1b1be18a7effc
Ricardo Jorge Jesus Da Costa Garcia <rjesus-d@c2r1s7.42porto.com>
2024-11-20 18:57:03 +0000

    Edited source file

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,8 @@
+#include "get_next_line.h"
+
+static char	*fill_line(int fd, char *left_c, char *buffer);
+static char	*set_line(char *line_buffer);
+
 char	*get_next_line(int fd)
 {
 	char	*buffer;
@@ -13,11 +18,60 @@ char	*get_next_line(int fd)
 	}
 	if (!buffer)
 		return (NULL);
-	line = //FUNCTION TO FILL LINE
+	line = fill_line(fd, left_c, buffer);
 	free(buffer);
 	buffer = NULL;
 	if (!line)
 		return (NULL);
-	left_c = //FUNCTION TO HANDLE LEFTOVERS
+	left_c = set_line(line);
 	return (line);
+}
+
+static char	*fill_line(int fd, char *left_c, char *buffer)
+{
+	ssize_t	bytes_read;
+	char	*tmp;
+
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(left_c);
+			return (NULL);
+		}
+		else if (bytes_read == 0)
+			break ;
+		buffer[bytes_read] = 0;
+		if (!left_c)
+			left_c = ft_strdup("");
+		tmp = left_c;
+		left_c = ft_strjoin(tmp, buffer);
+		free(tmp);
+		tmp = NULL;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (left_c);
+}
+
+static char	*set_line(char *line_buffer)
+{
+	char	*left_c;
+	ssize_t	i;
+
+	i = 0;
+	while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
+		i++;
+	if (line_buffer[i] == 0 || line_buffer[1] == 0)
+		return (NULL);
+	left_c = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i);
+	if (*left_c == 0)
+	{
+		free(left_c);
+		left_c = NULL;
+	}
+	line_buffer[i + 1] = 0;
+	return (left_c);
 }
